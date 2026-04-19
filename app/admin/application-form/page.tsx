3a0889d7e91025ed548a5e5f35f3802b5451c8e4
suhairii <suhairi14000@gmail.com>
2026-04-19 22:03:09 +0700

    fix: move use client directive to top

--- app/admin/application-form/page.tsx
+++ app/admin/application-form/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 
 import React, { useState } from "react";
 import CandidatesTable from "@/src/components/application-form/CandidatesTable";
