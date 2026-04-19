723fc081ef86b16c883183afd8d78e2565eb7011
suhairii <suhairi14000@gmail.com>
2026-04-19 21:59:15 +0700

    fix: final adjustment for Prisma 7 and Vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env["MONGODB_URI"],
+    url: process.env["DATABASE_URL"] || process.env["MONGODB_URI"],
   },
 });
